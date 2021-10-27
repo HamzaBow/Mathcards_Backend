afbb9ed28e5ed8ef166d6cd5c776c3f0abcc5ad6
Hamza Bow <hamzabow77@gmail.com>
2021-10-27 19:20:05 +0100

    feat: add `difficultyLevels` mongoose schema

--- models/card.js
+++ models/card.js
@@ -1,5 +1,31 @@
 const mongoose = require('mongoose');
 
+// const faceSchema = new mongoose.Schema({
+//   type
+// })
+const difficultyLevelsSchema = new mongoose.Schema({
+  veryEasy: {
+    type: Boolean,
+    required: false
+  },
+  easy: {
+    type: Boolean,
+    required: false
+  },
+  medium: {
+    type: Boolean,
+    required: false
+  },
+  hard: {
+    type: Boolean,
+    required: false
+  },
+  veryHard: {
+    type: Boolean,
+    required: false
+  },
+})
+
 const cardSchema = new mongoose.Schema({
   front: {
     type: Array,
@@ -8,6 +34,14 @@ const cardSchema = new mongoose.Schema({
   back: {
     type: Array,
     required: false
+  },
+  difficultyLevels: {
+    type: difficultyLevelsSchema,
+    required: false
+  },
+  tags: {
+    type: Array,
+    required: true
   },
   date: {
     type: Date,
