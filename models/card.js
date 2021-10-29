580a2b452b3ec35c614b322f18c4c9f00e4fb1aa
Hamza Bow <hamzabow77@gmail.com>
2021-10-29 10:33:40 +0100

    refactor: remove difficultyLevelsSchema

--- models/card.js
+++ models/card.js
@@ -1,31 +1,5 @@
 const mongoose = require('mongoose');
 
-// const faceSchema = new mongoose.Schema({
-//   type
-// })
-const difficultyLevelsSchema = new mongoose.Schema({
-  veryEasy: {
-    type: Boolean,
-    required: false
-  },
-  easy: {
-    type: Boolean,
-    required: false
-  },
-  medium: {
-    type: Boolean,
-    required: false
-  },
-  hard: {
-    type: Boolean,
-    required: false
-  },
-  veryHard: {
-    type: Boolean,
-    required: false
-  },
-})
-
 const cardSchema = new mongoose.Schema({
   front: {
     type: Array,
@@ -36,7 +10,7 @@ const cardSchema = new mongoose.Schema({
     required: false
   },
   difficultyLevels: {
-    type: difficultyLevelsSchema,
+    type: Object,
     required: false
   },
   tags: {
