b7142a4ee30f00a8c195680812e7096e31c10f2b
Hamza Bow <hamzabow77@gmail.com>
2021-11-04 15:00:41 +0100

    feat: add middleware and handle all requests to user

--- routes/users.js
+++ routes/users.js
@@ -13,28 +13,157 @@ router.get("/", async (req, res) => {
 });
 
 // Getting One
-router.get("/:id", (req, res) => {
-  res.send("User: " + req.params.id);
+router.get("/:id", getUser, async (req, res) => {
+  res.json(res.user)
 });
 
 // Creating One
 router.post("/", async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    following: req.body.following
-  })
   try {
-    const newUser = await user.save()
-    res.status(201).json(newUser)
+    const existingUser = await User.findOne({ uid: req.body.uid })
+    if(existingUser){
+      res.status(409).json({ message: 'User already exists'})
+    } else {
+      const user = new User({
+        uid: req.body.uid,
+        following: req.body.following,
+        collectionsIds: req.body.collectionsIds
+      })
+      const newUser = await user.save()
+      res.status(201).json(newUser)
+    }
   } catch (error) {
     res.status(400).json({ messge: error.message })
   }
 });
 
 // Updating One
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getUser, async (req, res) => {
+  res.user.name = req.body.name
+  res.user.following = req.body.following
+  res.user.collectionsIds = req.body.collectionsIds
+  try {
+    const updatedUser = await res.user.save()
+    res.json(updatedUser)
+  } catch (error) {
+    res.status(400).json({ message: error.message})
+  }
+});
+
+// Updating one with PATCH
+router.patch('/:id', getUser, async (req, res) => {
+  if (req.body.name != null) {
+    res.user.name = req.body.name
+  }
+  if (req.body.following != null) {
+    res.user.following = req.body.following
+  }
+  if (req.body.collectionsIds != null) {
+    res.user.collectionsIds = req.body.collectionsIds
+  }
+
+  try {
+    const updatedUser = await res.user.save()
+    res.json(updatedUser)
+  } catch (error) {
+    res.status(400).json({ message: error.message})
+  }
+}) 
 
 // Deleting One
-router.delete("/:id", (req, res) => {});
+router.delete('/:id', getUser, async (req, res) => {
+  try {
+    await res.user.remove()
+    res.json({ message: 'Deleted user'})
+  } catch (error) {
+    res.status(500).json({ message: error.message})
+  }
+}) 
+
+//***********************************************************
+//**********************   FOLLOWING   **********************
+//***********************************************************
+
+router.post('/:id/following', getUser, async (req, res) => {
+  try {
+    if(res.user.following.indexOf(req.body.followedId) === - 1){
+      res.user.following.push(req.body.followedId)
+      res.user.save()
+      res.status(201).json('follower added to user successfully')
+    } else {
+      res.status(409).json('follower already exists for this user')
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message})
+  }
+})
+
+// Deleting one follower
+router.delete('/:id/following', getUser, async (req, res) => {
+  try {
+    if(res.user.following.indexOf(req.body.followedId) !== - 1){
+      res.user.following = res.user.following.filter((id) => id !== req.body.followedId)
+      res.user.save()
+      res.json('followed deleted from user successfully')
+    } else {
+      res.status(404).json('followed does not exist in user')
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message})
+  }
+})
+
+//***********************************************************
+//***************   COLLECTIONS INSIDE USER   ***************
+//***********************************************************
+
+// Adding one collection to user
+router.post('/:id/collections', getUser, async (req, res) => {
+  try {
+    if(res.user.collectionsIds.indexOf(req.body.collectionId) === - 1){
+      res.user.collectionsIds.push(req.body.collectionId)
+      res.user.save()
+      res.status(201).json('collection added to user successfully')
+    } else {
+      res.status(409).json('collection already exists for this user')
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message})
+  }
+})
+
+// Deleting one collection from user.
+router.delete('/:id/collections', getUser, async (req, res) => {
+  try {
+    if(res.user.collectionsIds.indexOf(req.body.collectionId) !== - 1){
+      res.user.collectionsIds = res.user.collectionsIds.filter((id) => id !== req.body.collectionId)
+      res.user.save()
+      res.json('collection deleted from user successfully')
+    } else {
+      res.status(404).json('collection does not exist in user')
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message})
+  }
+})
+
+//***********************************************************
+//**********************   MIDDLEWARE   *********************
+//***********************************************************
+
+async function getUser(req, res, next) {
+  let user
+  try {
+    user = await User.findById(req.params.id)
+    if(user == null) {
+      return res.status(404).json({ message: "Cannot find user"})
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message})
+  }
+
+  res.user = user
+  next()
+}
 
 module.exports = router;
