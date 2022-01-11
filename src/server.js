cc574faea50f9c125dc93cf6459010320084c640
Hamza Bow <hamzabow77@gmail.com>
2022-01-11 19:50:57 +0100

    fix: use MONGODB_URI env variable instead of DATABASE_URL

--- src/server.js
+++ src/server.js
@@ -14,8 +14,8 @@ app.use(
 )
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI)
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.MONGODB_URI)
+// mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
